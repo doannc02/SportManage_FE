de7bf5c92f97bf921a60a5e2aad91ea6fd01d46a
anluu203 <151427161+anluu203@users.noreply.github.com>
2025-06-06 13:38:41 +0700

    fix bug review

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import {
   Box,
   chakra,
   Text,
   Image,
-  Button,
   Heading,
   Center,
   HStack,
@@ -20,79 +19,35 @@ import {
   AlertTitle,
 } from "@chakra-ui/react";
 import { FiShoppingCart } from "react-icons/fi";
-import axios from "axios";
 import { UserContext } from "../Contexts/UserContext";
-import { useNavigate } from "react-router-dom";
-import { getAppToken } from "../configs/token";
 import { useQueryProductsList } from "../services/customers/products";
 import { BASE_URL } from "../configs/auth";
-import { Empty } from "antd";
-// import { SearchContext } from "../Utilis/Context/SearchContext";
-// "https://makeup-api.herokuapp.com/api/v1/products.json"
-//https://skin-care-hub.onrender.com/product?l=all&q=
-const ProductPage = () => {
-  const { user, search, setSearch } = useContext(UserContext);
-  const [posts, setPosts] = useState([]);
-  const [noofElements, setNoofElements] = useState(10);
-  const [sortBy, setSortBy] = useState(null);
-  const navigate = useNavigate();
-  const tokenApp = getAppToken();
+import { Checkbox, Empty } from "antd";
+import useDetailProduct from "./admin/products/detail/useDetail";
+import { set } from "lodash";
 
-  const { data, isLoading, isError } = useQueryProductsList({
+const ProductPage = () => {
+  const { search, setSearch } = useContext(UserContext);
+  const [noofElements, setNoofElements] = useState(10);
+  const [selectCategory, setSelectCategory] = useState([]);
+  const [{ dataCategory }] = useDetailProduct();
+
+  const handleCategoryChange = (value) => {
+    setSelectCategory((prev) =>
+      prev.includes(value)
+        ? prev.filter((item) => item !== value)
+        : [...prev, value]
+    );
+  };
+  const { data, isLoading } = useQueryProductsList({
     pageNumber: 0,
     pageSize: 20,
     keyword: search,
+    categories: selectCategory,
   });
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setPosts(data?.items || []);
-      setNoofElements(10);
-      console.log("data in useEffect", data);
-    };
-    fetchProducts();
-  }, [data, isLoading]);
-
-  //setLoad(true);
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     //setLoad(true);
-
-  //     const res = await axios.get(
-  //       `https://BadmintonStore.onrender.com/product/search/${search}/${sortBy}`,
-  //       { withCredentials: true }
-  //     );
-  //     console.log(res.data.data);
-  //     setPosts(res.data.data);
-  //     setNoofElements(10);
-
-  //     setLoad(false);
-  //   };
-  //   fetchPosts();
-  // }, [sortBy, search]);
-
-  function handleQuickBuy(id) {
-    if (!tokenApp) {
-      return navigate("/login");
-    }
-    // navigate(`/CartPage/${id}`);
-    console.log(id);
-    id.user_id = user.id;
-    axios
-      .post(`https://BadmintonStore.onrender.com/product/cart`, id, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        // console.log(res.data.data)
-        // setCartId(res.data.data)
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  const slice = posts.slice(0, noofElements);
+  console.log(selectCategory);
+  
+  const slice = data?.items?.slice(0, noofElements);
   const loadMore = () => {
     setNoofElements(noofElements + noofElements);
   };
@@ -106,60 +61,26 @@ const ProductPage = () => {
     );
   }
   return (
-    <>
-      <SimpleGrid gap={10} gridTemplateColumns={"1fr 1fr"} p={2}>
-        <HStack display="grid" gridTemplateColumns={"0.5fr 1.5fr"} p={2}>
-          <Heading fontWeight="200">Category</Heading>
-          <Select
-            placeholder="Select option"
-            onChange={(e) => setSearch(e.target.value)}
-          >
-            <option value="eyeliner">Eyeliner</option>
-            <option value="foundation">Foundation</option>
-            <option value="eyeshadow">Eye Shadow</option>
-            <option value="lipstick">Lipstick</option>
-            <option value="mascara">Mascara</option>
-            <option value="bronzer">Bronzer</option>
-            <option value="nail polish">Nail Polish</option>
-            <option value="lip liner">Lip Liner</option>
-          </Select>
-        </HStack>
-        {/* <RadioGroup onChange={setProductype} value={productype}>
-            <Stack direction="row">
-              <Radio value="eyeliner">Eyeliner</Radio>
-              <Radio value="foundation">Foundation</Radio>
-            </Stack>
-          </RadioGroup> */}
-        <HStack>
-          <RadioGroup onChange={setSortBy} value={sortBy}>
-            <Stack direction="row">
-              <Radio value="asc" onClick={(e) => setSortBy("asc")}>
-                Price Low To High
-              </Radio>
-              <Radio value="desc" onClick={(e) => setSortBy("desc")}>
-                Price High To Low
-              </Radio>
-            </Stack>
-          </RadioGroup>
-        </HStack>
-        {/* WOrking Radio Button dont delete */}
-        {/*  <RadioGroup onChange={setProductype} value={productype}>
-            <Stack direction="row">
-              <Radio value="eyeliner">Eyeliner</Radio>
-              <Radio value="foundation">Foundation</Radio>
-              <Radio value="eyeshadow">Eye Shadow</Radio>
-              <Radio value="lipstick">Lipstick</Radio>
-              <Radio value="mascara">Mascara</Radio>
-              <Radio value="bronzer">Bronzer</Radio>
-              <Radio value="blush">Blush</Radio>
-              <Radio value="nail_polish">Nail Polish</Radio>
-              <Radio value="lip_liner">Lip Liner</Radio>
-            </Stack>
-          </RadioGroup> */}
-      </SimpleGrid>
+    <div className="flex gap-2">
+      <Box w="20%" display="flex" flexDirection="column" p={4} gap={2}>
+        <Heading as="h3" size="md" mb={4}>
+          Danh mục sản phẩm
+        </Heading>
+        <Stack spacing={3}>
+          {dataCategory?.items?.map((category) => (
+            <Checkbox
+              key={category.id}
+              checked={selectCategory.includes(category.id)}
+              onChange={() => handleCategoryChange(category.id)}
+            >
+              {category.name}
+            </Checkbox>
+          ))}
+        </Stack>
+      </Box>
       {slice.length !== 0 ? (
         <Box
-          w="95%"
+          w="80%"
           mx="auto"
           display="grid"
           gridTemplateColumns={["1fr", "1fr 1fr", "repeat(4, 1fr)"]}
@@ -245,7 +166,7 @@ const ProductPage = () => {
                         color="black"
                         fontSize="md"
                       >
-                        <chakra.a onClick={() => handleQuickBuy(el)}>
+                        <chakra.a>
                           <HStack spacing={2}>
                             <Icon as={FiShoppingCart} h={6} w={6} />
                             <Text fontSize="md" fontWeight="bold">
@@ -263,22 +184,7 @@ const ProductPage = () => {
       ) : (
         <Empty description={<span>Không có sản phẩm nào</span>} />
       )}
-
-      {/* <Button
-        fontWeight="600"
-        bgColor="black"
-        color="white"
-        borderRadius="0"
-        border={"2px solid black"}
-        _hover={{
-          bg: "cyan.500",
-        }}
-        onClick={() => loadMore()}
-        p={4}
-      >
-        Load More
-      </Button> */}
-    </>
+    </div>
   );
 };
 export default ProductPage;
