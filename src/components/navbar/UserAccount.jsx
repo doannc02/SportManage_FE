79643b313b7ef87897a8292c5adf88ec271f8c76
anluu203 <151427161+anluu203@users.noreply.github.com>
2025-07-08 22:41:02 +0700

    update navbar

--- src/components/navbar/UserAccount.jsx
+++ src/components/navbar/UserAccount.jsx
@@ -1,30 +1,21 @@
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  PopoverArrow,
-  PopoverCloseButton,
-  Flex,
-  Button,
-  Box,
-  Text,
-} from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
 import { RxPerson } from "react-icons/rx";
-import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import NavStyle from "./navbar.module.css";
 import { getAppToken } from "../../configs/token";
 import { logoutAccount } from "../../configs/axios";
-import { House, PackageCheck, Settings, UserRound } from "lucide-react";
+import { House, LogOut, PackageCheck, Settings, UserRound } from "lucide-react";
 import { DEFAULT_COLOR } from "../../const/enum";
 import { useQueryInfoCurrentCustomer } from "../../services/customers/current-infos";
-import { Avatar } from "antd";
-import { useEffect, useState } from "react";
+import { Avatar, Button, ConfigProvider, Divider, Dropdown } from "antd";
+import React, { useEffect, useState } from "react";
 
 function UserAccount() {
   const tokenApp = getAppToken();
   const { data } = useQueryInfoCurrentCustomer();
+  const navigate = useNavigate();
   const [isMobile, setIsMobile] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleResize = () => {
@@ -33,25 +24,29 @@ function UserAccount() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-  //   const { logout } = useAuth();
   const menuItems = [
-    { label: "Trang chủ", icon: <House strokeWidth={1} />, link: "/" },
+    {
+      label: "Trang chủ",
+      icon: <House strokeWidth={1} size={19} />,
+      key: "/",
+    },
     {
       label: "Đơn hàng",
-      icon: <PackageCheck strokeWidth={1} />,
-      link: "/order",
+      icon: <PackageCheck strokeWidth={1} size={19} />,
+      key: "/order",
     },
     {
       label: "Cá nhân",
-      icon: <UserRound strokeWidth={1} />,
-      link: "/customer-profile",
+      icon: <UserRound strokeWidth={1} size={19} />,
+      key: "/customer-profile",
     },
     {
       label: "Cài đặt",
-      icon: <Settings strokeWidth={1} />,
-      link: "/settings",
+      icon: <Settings strokeWidth={1} size={19} />,
+      key: "/settings",
     },
   ];
+
   if (isMobile) {
     return (
       <Flex
@@ -67,12 +62,11 @@ function UserAccount() {
           bg={data?.avatarUrl ? "" : "teal.500"}
           color="white"
           borderRadius="full"
-          p={data.avatarUrl ? 0 : 1.5}
+          p={data?.avatarUrl ? 0 : 1.5}
           display="flex"
           alignItems="center"
           justifyContent="center"
         >
-          {/* <RxPerson size="22px" /> */}
           {data?.avatarUrl ? (
             <Avatar size={50} src={data?.avatarUrl} />
           ) : (
@@ -89,18 +83,44 @@ function UserAccount() {
       </Flex>
     );
   }
+
   return (
-    <Popover trigger="hover" placement="bottom-end">
-      <PopoverTrigger>
-        <Flex
-          alignItems="center"
-          gap="8px"
-          cursor="pointer"
-          px={3}
-          py={2}
-          className={NavStyle.userAccountTrigger}
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: DEFAULT_COLOR,
+            controlItemBgActive: "#d5f0ef",
+            controlItemBgActiveHover: "rgba(213, 240, 239)",
+          },
+        }}
+      >
+        <Dropdown
+          menu={{
+            items: menuItems,
+            selectable: true,
+            defaultSelectedKeys: [location.pathname],
+            onClick: (e) => navigate(e.key),
+          }}
+          popupRender={(menu) => (
+            <div>
+              {React.cloneElement(menu)}
+              <Divider style={{ margin: 0 }} />
+              <div style={{ padding: 4 }}>
+                <Button
+                  onClick={() => logoutAccount()}
+                  icon={<LogOut strokeWidth={1} size={15} />}
+                  type="primary"
+                  size="large"
+                >
+                  Đăng xuất
+                </Button>
+              </div>
+            </div>
+          )}
         >
           <Box
+            onClick={(e) => e.preventDefault()}
             bg={data?.avatarUrl ? "" : "teal.500"}
             color="white"
             borderRadius="full"
@@ -109,76 +129,15 @@ function UserAccount() {
             alignItems="center"
             justifyContent="center"
           >
-            {/* <RxPerson size="22px" /> */}
             {data?.avatarUrl ? (
               <Avatar size={50} src={data?.avatarUrl} />
             ) : (
               <RxPerson size="22px" />
             )}
           </Box>
-          <Text
-            fontWeight="medium"
-            fontSize="md"
-            display={{ lg: "block", md: "none", sm: "none", base: "none" }}
-          >
-            {tokenApp.username}
-          </Text>
-        </Flex>
-      </PopoverTrigger>
-      <PopoverContent
-        w="250px"
-        borderRadius="lg"
-        boxShadow="lg"
-        p={0}
-        bg="white"
-      >
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverBody p={5}>
-          <Flex flexDir="column" gap={3}>
-            <Flex direction="column" gap={2} mt={4}>
-              {menuItems.map((item) => (
-                <Link to={item?.link} key={item.label}>
-                  <Button
-                    variant="ghost"
-                    w="100%"
-                    justifyContent="flex-start"
-                    leftIcon={item?.icon}
-                    fontWeight="medium"
-                    fontSize="md"
-                    color={
-                      location.pathname === item.link
-                        ? DEFAULT_COLOR
-                        : "gray.700"
-                    }
-                    bg={
-                      location.pathname === item.link
-                        ? "green.50"
-                        : "transparent"
-                    }
-                    _hover={{ bg: "green.50", color: DEFAULT_COLOR }}
-                  >
-                    {item.label}
-                  </Button>
-                </Link>
-              ))}
-            </Flex>
-            <Button
-              colorScheme="teal"
-              variant="solid"
-              mt={2}
-              borderRadius="md"
-              onClick={() => {
-                logoutAccount();
-              }}
-              _hover={{ bg: "teal.600" }}
-            >
-              Đăng xuất
-            </Button>
-          </Flex>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+        </Dropdown>
+      </ConfigProvider>
+    </>
   );
 }
 
